1378a406370e577de6bbdda823e882675105c750
maalloor <maalloor@espol.edu.ec>
2022-05-28 20:58:50 -0500

    Survey simple by Manuel Loor

---
+++ my_survey/__init__.py
@@ -0,0 +1,31 @@
+from otree.api import *
+
+
+doc = """
+My survey by Manuel Loor
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'my_survey'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+class Subsession(BaseSubsession):
+    pass
+
+class Group(BaseGroup):
+    pass
+
+class Player(BasePlayer):
+    name = models.StringField(label='What is your name?', widget=widgets.TextInput)
+    age = models.IntegerField(label='What is your age?', min=13, max=125)
+
+class Survey(Page):
+    form_model = 'player'
+    form_fiels = ['name', 'age']
+
+class Results(Page):
+    form_model = 'player'
+
+page_sequence = [Survey, Results]
