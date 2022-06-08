1cd6059fc3e9235378489ed2a7ff69bf88346366
maalloor <maalloor@espol.edu.ec>
2022-06-08 10:06:40 -0500

    Bug fix in My survey experiment

--- my_survey/__init__.py
+++ my_survey/__init__.py
@@ -1,31 +1,36 @@
+"""Module otree"""
 from otree.api import *
 
 
-doc = """
+DOC = """
 My survey by Manuel Loor
 """
 
 
 class C(BaseConstants):
+    """Class representing Especial"""
     NAME_IN_URL = 'my_survey'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
 
 class Subsession(BaseSubsession):
-    pass
+    """Class representing a Subsession"""
 
 class Group(BaseGroup):
-    pass
+    """Class representing Group"""
 
 class Player(BasePlayer):
+    """Class representing a Player"""
     name = models.StringField(label='What is your name?')
     age = models.IntegerField(label='What is your age?')
 
 class Survey(Page):
+    """Class representing Survey"""
     form_model = 'player'
     form_fiels = ['name', 'age']
 
 class Results(Page):
+    """Class representing last result"""
     form_model = 'player'
 
 page_sequence = [Survey, Results]
